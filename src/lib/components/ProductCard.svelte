055b28c0c0250d78066fffc2c058db3a3c6b5ba0
kurt <ttwishing@gmail.com>
2025-12-04 18:14:32 +0800

    refactor product card

--- src/lib/components/ProductCard.svelte
+++ src/lib/components/ProductCard.svelte
@@ -37,12 +37,10 @@
 	</div>
 	<div class="p-3 lg:p-6">
 		<h3 class="text-title mb-3">{product.name}</h3>
-		<p class="mb-3">{product.descrition}</p>
-		<div class="grid grid-cols-2 gap-2">
+		<p class="mb-6">{product.descrition}</p>
+		<div class="grid grid-cols-2 gap-3">
 			{#each Object.entries(product.params) as [key, value]}
-				<div
-					class="px-3 py-2 bg-surface-container-high borderborder border-outline rounded-lg text-left"
-				>
+				<div class="px-3 py-2 bg-white/2 border border-white/3 rounded-lg text-left">
 					<p class="text-sm">
 						{key}
 					</p>
