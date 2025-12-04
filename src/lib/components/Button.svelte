7ab61e9be2bb2bdce315a97f48ace17652342827
kurt <ttwishing@gmail.com>
2025-12-04 16:42:20 +0800

    refactor button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -27,17 +27,21 @@
 
 	const variantClasses: Record<NonNullable<ButtonProps['variant']>, string> = {
 		primary:
-			'bg-primary/70 text-white/80 shadow-theme-xs hover:bg-primary/60 disabled:bg-primary/30',
-		outline: 'bg-white text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+			'inline-flex grow rounded-lg bg-primary px-8 text-on-background  focus:outline-transparent focus-visible:outline focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-solar-500 disabled:bg-surface-container-highest disabled:text-on-surface',
+		outline:
+			'flex grow rounded-lg px-8  text-primary ring-2 ring-inset ring-outline focus-visible:outline-hidden focus-visible:ring-solar-500  hover:active:ring-outline  disabled:text-on-surface  disabled:hover:bg-transparent disabled:hover:ring-on-surface/12 disabled:ring-on-surface/12'
 	};
 
 	const buttonClasses = $derived(
-		`inline-flex items-center justify-center gap-2 rounded-lg transition ${className} ${
+		`group/button relative cursor-pointer items-center justify-center text-center text-base font-medium text-nowrap transition-opacity disabled:cursor-not-allowed disabled:opacity-30 ${className} ${
 			sizeClasses[size]
-		} ${variantClasses[variant]} ${disabled ? 'cursor-not-allowed opacity-50' : ''}`
+		} ${variantClasses[variant]}'}`
 	);
 </script>
 
 <button class={buttonClasses} onclick={onClick} {disabled}>
+	<div
+		class="state-layer pointer-events-none absolute inset-0 rounded-[inherit] bg-current opacity-0 transition-opacity group-hover/button:opacity-8 group-focus-visible/button:opacity-10 group-active/button:group-hover/button:opacity-16 group-disabled/button:hidden"
+	></div>
 	{@render children()}
 </button>
